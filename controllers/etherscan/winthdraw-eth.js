c51b4e89aef74607054b30954c7ab543d7faf528
keyrwinz <keyrwinfelisilda@gmail.com>
2021-06-14 11:15:05 +0800

    update withdraw functions

--- controllers/etherscan/winthdraw-eth.js
+++ controllers/etherscan/winthdraw-eth.js
@@ -14,12 +14,18 @@ const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
 const INFURA_PRIVATE_KEY = process.env.INFURA_PROJECT_KEY
 const NETWORK_CHAIN = IS_DEV ? process.env.ROPSTEN_TESTNET : process.env.ETH_MAINNET
 
+const TEMP_NONCE_EXT_DATA = "https://api.jsonbin.io/b/60c1f2b192164b68bec82608"
+const headers = { 
+  "secret-key": "$2b$10$FEpBX2bZ9BhQ3EoXWDCnteJiMCiuo246Vp1Zdyj32OBRCeqrQoES.",
+  "versioning": false
+}
+
 const GAS_PRICE_URL = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_KEY}`
 
 exports.withdrawEther = async (req, res) => {
-  const { toHex, toWei } = Web3.utils;
+  const { toHex, toWei, fromWei } = Web3.utils;
   try {
-    const { address, value } = req.body;
+    const { address, value  } = req.body;
 
     if (!address || !value) {
       return res.status(400).json({
@@ -31,30 +37,51 @@ exports.withdrawEther = async (req, res) => {
     const web3 = new Web3(new Web3.providers.HttpProvider(`https://:${INFURA_PRIVATE_KEY}@${NETWORK_CHAIN}.infura.io/v3/${INFURA_PROJECT_ID}`));
     
     const { data } = await axios.get(GAS_PRICE_URL)
-    const gasPrice = toWei(data.result.FastGasPrice, "gwei")
+    // custon nonce data
+    // const { data: { nonce }} = await axios.get(TEMP_NONCE_EXT_DATA, { headers })
+    // nonce based on latest block
+    const nonce = await web3.eth.getTransactionCount(META_ADDRESS)
 
-    // const parsedValue = parseFloat(toWei((value + ""), 'ether'))
+    const gasPrice = toWei(data.result.FastGasPrice, "gwei")
+    const parsedValue = toWei(value.toString(), "ether")
+    // const gasPriceInEth = fromWei(gasPrice, "ether")
+
     const rawTx = {
-      gasPrice: toHex(gasPrice),
+      nonce,
+      gasPrice: toHex(+gasPrice * 1.5),
+      gasLimit: toHex("100000"),
       from: META_ADDRESS,
       to: address,
-      value: toHex(toWei(value.toString(), "ether")),
-      data: "0x"
+      value: toHex(parsedValue),
+      data: "0x",
     }
+
+    console.log(chalk.green('Sending transaction (ETH): '), rawTx)
 
     const tx = new Tx(rawTx, { chain: NETWORK_CHAIN });
     tx.sign(PRIVATE_KEY);
-    
     const serializedTx = tx.serialize();
-    
+
     web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-    .then(response => console.log({ response }))
-    .catch((error) => {
-      console.log({ transactionErr: error })
-      return res.status(400).json({
+    .once('transactionHash', (txHash) => {
+      // console.log({ txHash })
+      // axios.put(TEMP_NONCE_EXT_DATA, { nonce: (+nonce + 1) }, { headers })
+      // .then(res => console.log("UPDATED NONCE TO: ", +nonce + 1))
+      // .catch(err => console.log("ERROR UPDATING NONCE", err))
+      // console.log("DONE UPDATING")
+      res.status(200).json({
+        error: false,
+        message: "success",
+        tx_hash: txHash,
+      })
+    })
+    .catch(error => {
+      console.log({ sendSignedTransaction_ERROR: error })
+      res.status(400).json({
+        from: "sendSignedTransaction",
         error: true,
-        message: error,
-      });
+        message: error.message
+      })
     })
 
   } catch (error) {
@@ -62,7 +89,7 @@ exports.withdrawEther = async (req, res) => {
 
     return res.status(400).json({
       error: true,
-      message: error,
+      message: error.msg || error.message || error,
     });
   }
 };
--- routes/etherscan/index.js
+++ routes/etherscan/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 const {
   getTransactionByHash,
 } = require("../../controllers/etherscan/transaction_by_hash");
-const { withdrawEther } = require("../../controllers/etherscan/winthdraw");
+const { withdrawEther } = require("../../controllers/etherscan/winthdraw-eth");
+const { withdrawUSDC } = require("../../controllers/etherscan/winthdraw-usdc");
 const validate = require("../../validation/etherscan_validation");
 
 router.get("/", (_, res) => res.send("EGS ETHERSCAN API"));
 
 router.get("/txhash-status", validate.txHash, getTransactionByHash);
-router.get("/withdraw-eth", withdrawEther);
+router.post("/withdraw-eth", withdrawEther);
+router.post("/withdraw-usdc", withdrawUSDC);
 
 module.exports = router;
