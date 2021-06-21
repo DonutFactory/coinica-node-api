33e1b07cb40a8dfc01544a4df422031b1a16dfb4
Roelan <roelanyburan@gmail.com>
2021-06-21 17:12:13 +0800

    remove ws in etherscan

--- controllers/etherscan/transaction_by_hash.js
+++ controllers/etherscan/transaction_by_hash.js
@@ -1,5 +1,4 @@
 const chalk = require("chalk");
-const WebSocket = require("ws");
 
 const { eth_getTransactionByHash } = require("../../services/etherscan_api");
 
@@ -7,28 +6,11 @@ exports.getTransactionByHash = async (req, res) => {
   try {
     const {
       body: { tx_hash, currency },
-      wsServerApi,
     } = req;
     const response = await eth_getTransactionByHash({ tx_hash, currency });
 
-    const result = { currency, ...response };
-
-    console.log("try init wsServerApi", wsServerApi)
-    console.log("try init wsServerApi readyState", wsServerApi.readyState)
-
-    //broadcast the response to server
-    if (wsServerApi && wsServerApi.readyState === WebSocket.OPEN) {
-      wsServerApi.send(JSON.stringify(result));
-    }
-    else {
-      console.log("cannot send message to websocket server");
-    }
-
-    return res.json(result);
+    return res.json({ currency, ...response });
   } catch (error) {
-    console.log(chalk.red(error));
-    console.log("catch init wsServerApi", wsServerApi)
-    console.log("catch init wsServerApi readyState", wsServerApi.readyState)
 
     return res.status(400).json({
       error: true,
