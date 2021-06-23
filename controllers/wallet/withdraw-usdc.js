8c1a42a584d446a48fd7b46038ae62fa8f325a4e
Roelan <roelanyburan@gmail.com>
2021-06-23 17:01:52 +0800

    test usdc

--- controllers/wallet/withdraw-usdc.js
+++ controllers/wallet/withdraw-usdc.js
@@ -25,106 +25,114 @@ const GAS_PRICE_URL = `https://api.etherscan.io/api?module=gastracker&action=gas
 const ROPSTEN_USDC_ADDRESS = "0x07865c6e87b9f70255377e024ace6630c1eaa37f"
 
 exports.withdrawUSDC = async (req, res) => {
-  const { toHex, toWei, fromWei } = Web3.utils;
-  try {
-    const { body: { address, value, gasPrice, account_id }, wsServerApi } = req;
+  const { body: { address, value, gasPrice, account_id }, wsServerApi } = req;
+  return res.json({
+    status: 1,
+    address, value, gasPrice, account_id
+  })
+}
 
-    if (!address || !value || !gasPrice || !account_id) {
-      return res.status(400).json({
-        error: true,
-        message: 'Required parameters: address, value, gasPrice, account_id'
-      })
-    }
+// exports.withdrawUSDC = async (req, res) => {
+//   const { toHex, toWei, fromWei } = Web3.utils;
+//   try {
+//     const { body: { address, value, gasPrice, account_id }, wsServerApi } = req;
 
-    const web3 = new Web3(new Web3.providers.HttpProvider(`https://:${INFURA_PRIVATE_KEY}@${NETWORK_CHAIN}.infura.io/v3/${INFURA_PROJECT_ID}`));
+//     if (!address || !value || !gasPrice || !account_id) {
+//       return res.status(400).json({
+//         error: true,
+//         message: 'Required parameters: address, value, gasPrice, account_id'
+//       })
+//     }
 
-    const encodedAbi = web3.eth.abi.encodeFunctionCall({
-      name: 'transfer',
-      type: 'function',
-      inputs: [{
-        type: 'address',
-        name: '_to'
-      }, {
-        type: 'uint256',
-        name: '_value'
-      }]
-    }, [address, `${(+value) * 1000000}`]);
+//     const web3 = new Web3(new Web3.providers.HttpProvider(`https://:${INFURA_PRIVATE_KEY}@${NETWORK_CHAIN}.infura.io/v3/${INFURA_PROJECT_ID}`));
+
+//     const encodedAbi = web3.eth.abi.encodeFunctionCall({
+//       name: 'transfer',
+//       type: 'function',
+//       inputs: [{
+//         type: 'address',
+//         name: '_to'
+//       }, {
+//         type: 'uint256',
+//         name: '_value'
+//       }]
+//     }, [address, `${(+value) * 1000000}`]);
 
 
-    // const { data } = await axios.get(GAS_PRICE_URL)
-    // const gasPrice = toWei(data.result.FastGasPrice, "gwei")
+//     // const { data } = await axios.get(GAS_PRICE_URL)
+//     // const gasPrice = toWei(data.result.FastGasPrice, "gwei")
 
-    // custon nonce data
-    // const { data: { nonce }} = await axios.get(TEMP_NONCE_EXT_DATA, { headers })
-    // nonce based on latest block
-    const nonce = await web3.eth.getTransactionCount(META_ADDRESS)
-    // const parsedValue = toWei(value.toString(), "ether")
-    // const gasPriceInEth = fromWei(gasPrice, "ether")
+//     // custon nonce data
+//     // const { data: { nonce }} = await axios.get(TEMP_NONCE_EXT_DATA, { headers })
+//     // nonce based on latest block
+//     const nonce = await web3.eth.getTransactionCount(META_ADDRESS)
+//     // const parsedValue = toWei(value.toString(), "ether")
+//     // const gasPriceInEth = fromWei(gasPrice, "ether")
 
-    const rawTx = {
-      nonce,
-      gasPrice: toHex(gasPrice),
-      gasLimit: toHex("500000"),
-      from: META_ADDRESS,
-      to: ROPSTEN_USDC_ADDRESS,
-      data: encodedAbi,
-    }
+//     const rawTx = {
+//       nonce,
+//       gasPrice: toHex(gasPrice),
+//       gasLimit: toHex("500000"),
+//       from: META_ADDRESS,
+//       to: ROPSTEN_USDC_ADDRESS,
+//       data: encodedAbi,
+//     }
 
-    console.log(chalk.green('Sending transaction (USDC): '), rawTx)
+//     console.log(chalk.green('Sending transaction (USDC): '), rawTx)
 
-    const tx = new Tx(rawTx, { chain: NETWORK_CHAIN });
-    tx.sign(PRIVATE_KEY);
-    const serializedTx = tx.serialize();
+//     const tx = new Tx(rawTx, { chain: NETWORK_CHAIN });
+//     tx.sign(PRIVATE_KEY);
+//     const serializedTx = tx.serialize();
 
-    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-      .once('transactionHash', async (txHash) => {
-        // await axios.put(TEMP_NONCE_EXT_DATA, { nonce: (+nonce + 1) }, { headers })
-        // .then(res => console.log("UPDATED NONCE TO: ", +nonce + 1))
-        // .catch(err => console.log("ERROR UPDATING NONCE", err))
+//     web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+//       .once('transactionHash', async (txHash) => {
+//         // await axios.put(TEMP_NONCE_EXT_DATA, { nonce: (+nonce + 1) }, { headers })
+//         // .then(res => console.log("UPDATED NONCE TO: ", +nonce + 1))
+//         // .catch(err => console.log("ERROR UPDATING NONCE", err))
 
-        // Notify server api via websocket about user withdraw
-        if (wsServerApi.readyState === WebSocket.OPEN) {
-          const result = {
-            account_id: account_id,
-            tx_hash: txHash,
-            tx_type: "WITHDRAW",
-            currency: "USDC"
-          }
-          wsServerApi.send(JSON.stringify(result));
-        }
-        // Note: status: 0 = Fail, 1 = Pass
-        return res.status(200).json({
-          status: 1,
-        })
-      })
-      .catch(error => {
-        console.log({ sendSignedTransaction_ERROR: error })
+//         // Notify server api via websocket about user withdraw
+//         if (wsServerApi.readyState === WebSocket.OPEN) {
+//           const result = {
+//             account_id: account_id,
+//             tx_hash: txHash,
+//             tx_type: "WITHDRAW",
+//             currency: "USDC"
+//           }
+//           wsServerApi.send(JSON.stringify(result));
+//         }
+//         // Note: status: 0 = Fail, 1 = Pass
+//         return res.status(200).json({
+//           status: 1,
+//         })
+//       })
+//       .catch(error => {
+//         console.log({ sendSignedTransaction_ERROR: error })
 
-        //Notify if transaction has error
-        if (wsServerApi.readyState === WebSocket.OPEN) {
-          const result = {
-            tx_hash: txHash,
-            tx_type: "WITHDRAW",
-            currency: "USDC",
-            account_id: account_id
-          }
-          wsServerApi.send(JSON.stringify(result));
-        }
+//         //Notify if transaction has error
+//         if (wsServerApi.readyState === WebSocket.OPEN) {
+//           const result = {
+//             tx_hash: txHash,
+//             tx_type: "WITHDRAW",
+//             currency: "USDC",
+//             account_id: account_id
+//           }
+//           wsServerApi.send(JSON.stringify(result));
+//         }
 
-        return res.status(400).json({
-          message: "transaction failed",
-          error: error,
-          status: 0,
-        });
-      })
+//         return res.status(400).json({
+//           message: "transaction failed",
+//           error: error,
+//           status: 0,
+//         });
+//       })
 
-  } catch (error) {
-    console.log(chalk.red(error));
+//   } catch (error) {
+//     console.log(chalk.red(error));
 
-    return res.status(400).json({
-      message: "something went wrong",
-      error: error,
-      status: 0,
-    });
-  }
-};
+//     return res.status(400).json({
+//       message: "something went wrong",
+//       error: error,
+//       status: 0,
+//     });
+//   }
+// };
