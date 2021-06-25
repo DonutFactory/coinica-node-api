d92d533c01ab70a32ee33f14cde7d6013de6c38d
Roelan <roelanyburan@gmail.com>
2021-06-25 15:44:23 +0800

    update

--- controllers/wallet/withdraw-eth.js
+++ controllers/wallet/withdraw-eth.js
@@ -1,43 +1,52 @@
-const Web3 = require('web3');
-const axios = require('axios');
+const Web3 = require("web3");
+const axios = require("axios");
 const chalk = require("chalk");
-const Tx = require('ethereumjs-tx').Transaction;
+const Tx = require("ethereumjs-tx").Transaction;
 const WebSocket = require("ws");
+const { toFixed } = require("../../utils");
 
-const IS_DEV = process.env.NODE_ENV === "development"
+const IS_DEV = process.env.NODE_ENV === "development";
 
 const ETHERSCAN_KEY = process.env.ETHERSCAN_API_KEY;
 
 const META_ADDRESS = process.env.METAMASK_ADDESS;
-const PRIVATE_KEY = Buffer.from(process.env.METAMASK_PRIVATE_KEY, 'hex');
+const PRIVATE_KEY = Buffer.from(process.env.METAMASK_PRIVATE_KEY, "hex");
 
-const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
-const INFURA_PRIVATE_KEY = process.env.INFURA_PROJECT_KEY
-const NETWORK_CHAIN = IS_DEV ? process.env.ROPSTEN_TESTNET : process.env.ETH_MAINNET
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const INFURA_PRIVATE_KEY = process.env.INFURA_PROJECT_KEY;
+const NETWORK_CHAIN = IS_DEV
+  ? process.env.ROPSTEN_TESTNET
+  : process.env.ETH_MAINNET;
 
-const TEMP_NONCE_EXT_DATA = "https://api.jsonbin.io/b/60c1f2b192164b68bec82608"
+const TEMP_NONCE_EXT_DATA = "https://api.jsonbin.io/b/60c1f2b192164b68bec82608";
 const headers = {
   "secret-key": "$2b$10$FEpBX2bZ9BhQ3EoXWDCnteJiMCiuo246Vp1Zdyj32OBRCeqrQoES.",
-  "versioning": false
-}
+  versioning: false,
+};
 
-const GAS_PRICE_URL = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_KEY}`
+const GAS_PRICE_URL = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_KEY}`;
 
 exports.withdrawEther = async (req, res) => {
   const { toHex, toWei, fromWei } = Web3.utils;
-  const { body: { address, value, gasPrice, account_id }, wsServerApi } = req;
+  const {
+    body: { address, value, gasPrice, account_id },
+    wsServerApi,
+  } = req;
   try {
-
     // const { body: { address, value, gasPrice, account_id }, wsServerApi } = req;
 
     if (!address || !value || !gasPrice || !account_id) {
       return res.status(400).json({
         error: true,
-        message: 'Required parameters: address, value, gasPrice, account_id'
-      })
+        message: "Required parameters: address, value, gasPrice, account_id",
+      });
     }
 
-    const web3 = new Web3(new Web3.providers.HttpProvider(`https://:${INFURA_PRIVATE_KEY}@${NETWORK_CHAIN}.infura.io/v3/${INFURA_PROJECT_ID}`));
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        `https://:${INFURA_PRIVATE_KEY}@${NETWORK_CHAIN}.infura.io/v3/${INFURA_PROJECT_ID}`
+      )
+    );
 
     // const { data } = await axios.get(GAS_PRICE_URL)
     // const gasPrice = toWei(data.result.FastGasPrice, "gwei")
@@ -45,29 +54,32 @@ exports.withdrawEther = async (req, res) => {
     // custon nonce data
     // const { data: { nonce }} = await axios.get(TEMP_NONCE_EXT_DATA, { headers })
     // nonce based on latest block
-    const nonce = await web3.eth.getTransactionCount(META_ADDRESS.toLowerCase())
+    const nonce = await web3.eth.getTransactionCount(
+      META_ADDRESS.toLowerCase()
+    );
 
-    const parsedValue = toWei(value.toString(), "ether")
+    const parsedValue = toWei(value.toString(), "ether");
     // const gasPriceInEth = fromWei(gasPrice, "ether")
 
     const rawTx = {
       nonce,
-      gasPrice: toHex(gasPrice),
+      gasPrice: toHex(toFixed(gasPrice)),
       gasLimit: toHex("500000"),
       from: META_ADDRESS.toLowerCase(),
       to: address.toLowerCase(),
       value: toHex(parsedValue),
       data: "0x",
-    }
+    };
 
-    console.log(chalk.green('Sending transaction (ETH): '), rawTx)
+    console.log(chalk.green("Sending transaction (ETH): "), rawTx);
 
     const tx = new Tx(rawTx, { chain: NETWORK_CHAIN });
     tx.sign(PRIVATE_KEY);
     const serializedTx = tx.serialize();
 
-    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-      .once('transactionHash', (txHash) => {
+    web3.eth
+      .sendSignedTransaction("0x" + serializedTx.toString("hex"))
+      .once("transactionHash", (txHash) => {
         // console.log({ txHash })
         // axios.put(TEMP_NONCE_EXT_DATA, { nonce: (+nonce + 1) }, { headers })
         // .then(res => console.log("UPDATED NONCE TO: ", +nonce + 1))
@@ -80,17 +92,17 @@ exports.withdrawEther = async (req, res) => {
             account_id: account_id,
             tx_hash: txHash,
             tx_type: "WITHDRAW",
-            currency: "ETH"
-          }
+            currency: "ETH",
+          };
           wsServerApi.send(JSON.stringify(result));
         }
         // Note: status: 0 = Fail, 1 = Pass
         return res.status(200).json({
           status: 1,
-        })
+        });
       })
-      .catch(error => {
-        console.log({ sendSignedTransaction_ERROR: error })
+      .catch((error) => {
+        console.log({ sendSignedTransaction_ERROR: error });
 
         //Notify if transaction has error
         if (wsServerApi.readyState === WebSocket.OPEN) {
@@ -98,16 +110,15 @@ exports.withdrawEther = async (req, res) => {
             tx_hash: txHash,
             tx_type: "WITHDRAW",
             currency: "ETH",
-            account_id: account_id
-          }
+            account_id: account_id,
+          };
           wsServerApi.send(JSON.stringify(result));
         }
 
         return res.status(400).json({
           status: 0,
         });
-      })
-
+      });
   } catch (error) {
     console.log("erorrz:", chalk.red(error));
     console.log("error body params: ", req.body);
