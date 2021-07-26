7234838c2bad747d5f4b1f230b8585b34fab9a54
Roelan <roelanyburan@gmail.com>
2021-07-26 17:09:55 +0800

    scripts to dynamically change env

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (_, res) => {
-  res.send('EGSNET NODE SERVER')
+  res.send('EGSNET NODE SERVER ' + process.env.NODE_ENV);
 });
 
 module.exports = router;
